78d736c448f9059523a18720549ce2b3082aacef
yodaum <yod.aum@gmail.com>
2023-02-23 15:58:37 +0000

    Completed instantiation Lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        String newStringObject = new String();
+        return newStringObject ;
     }
 }
